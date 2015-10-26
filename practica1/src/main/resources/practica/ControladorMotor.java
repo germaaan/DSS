5fd92e34ad62a2486c363dadc9aecb34d86831cc
German Martinez <germaaan@gmail.com>
2015-10-26 14:14:06 +0100

    Construcción de página de inicio lista

--- practica1/src/main/resources/practica/ControladorMotor.java
+++ practica1/src/main/resources/practica/ControladorMotor.java
@@ -17,19 +17,52 @@ public class ControladorMotor implements Serializable {
     @ManagedProperty(value = "#{textoBotonOnOff}")
     private String textoBotonOnOff;
 
+    @ManagedProperty(value = "#{colorTitulo}")
+    private String colorTitulo;
+
+    @ManagedProperty(value = "#{colorBotonOnOff}")
+    private String colorBotonOnOff;
+
+    @ManagedProperty(value = "#{colorBotonAcelerar}")
+    private String colorBotonAcelerar;
+
     public String getTextoTitulo() {
         return "APAGADO";
-    }
-
-    public void setTextoTitulo(String textoTitulo) {
-        this.textoTitulo = textoTitulo;
     }
 
     public String getTextoBotonOnOff() {
         return "Encender";
     }
 
+    public String getColorTitulo() {
+        return "red";
+    }
+
+    public String getColorBotonOnOff() {
+        return "red";
+    }
+
+    public String getColorBotonAcelerar() {
+        return "black";
+    }
+
+    public void setTextoTitulo(String textoTitulo) {
+        this.textoTitulo = textoTitulo;
+    }
+
     public void setTextoBotonOnOff(String textoBotonOnOff) {
         this.textoBotonOnOff = textoBotonOnOff;
+    }
+
+    public void setColorTitulo(String colorTitulo) {
+        this.colorTitulo = colorTitulo;
+    }
+
+    public void setColorBotonOnOff(String colorBotonOnOff) {
+        this.colorBotonOnOff = colorBotonOnOff;
+    }
+
+    public void setColorBotonAcelerar(String colorBotonAcelerar) {
+        this.colorBotonAcelerar = colorBotonAcelerar;
     }
 }
